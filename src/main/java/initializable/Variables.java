c8c3bd2fe0a6f44a2b18aaac75d5f7fa88eaa9b7
JasonLong24 <jasonlongball@gmail.com>
2018-01-16 21:11:42 -0500

    Big changes to file path and starting to add gradle build

---
+++ src/main/java/initializable/Variables.java
@@ -0,0 +1,19 @@
+package main.java.initializable;
+
+public class Variables {
+
+	// Variables used for spaced repetition algorithm
+	public static double time1 = 0;
+	public static double time2 = .041;
+	public static double factor = 0;
+	public static double memStrength = 1;
+	public static int n = 0;
+	public static double d = 0;
+
+	// Scene dimensions
+	public static int width = 600;
+	public static int height = 400;
+	
+	// Score
+	public static int score = 50;
+}
---
+++ src/main/java/rewardSystem/RewardController.java
@@ -0,0 +1,57 @@
+package main.java.rewardSystem;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import main.java.initializable.Methods;
+import main.java.initializable.Variables;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+
+public class RewardController implements Initializable {
+	
+	@FXML
+	private ListView<String> rewardItems;
+	@FXML
+	public Label rewardScoreKeep;
+	
+	private static ObservableList<String> rewardItem = FXCollections.observableArrayList("Placeholder Item 1", "Placeholder Item 2", "Placeholder Item 3");
+	
+	public void getRewardBuy(MouseEvent e) {
+		int getRewardSelection = rewardItems.getSelectionModel().getSelectedIndex();
+		System.out.println(getRewardSelection);
+		
+		switch(getRewardSelection) {
+		case 0:
+			if(Variables.score >= 1) {
+				Methods.getBuyNotification(1);
+				rewardScoreKeep.setText(String.valueOf(Variables.score));
+			}else {Methods.noFunds();}
+			break;
+		case 1:
+			if(Variables.score >= 2) {
+				Methods.getBuyNotification(2);
+				rewardScoreKeep.setText(String.valueOf(Variables.score));
+			}else {Methods.noFunds();}
+			break;
+		case 2:
+			if(Variables.score >= 3) {
+				Methods.getBuyNotification(3);
+				rewardScoreKeep.setText(String.valueOf(Variables.score));
+			}else {Methods.noFunds();}
+		default:
+			break;
+		}
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		rewardScoreKeep.setText(String.valueOf(Variables.score));
+		rewardItems.setItems(rewardItem);
+	}
+}
